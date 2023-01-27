a63b596885f3ae335b4f7b0b49982f1f5cafd608
Dylan <gigantedylan001@yahoo.com>
2023-01-26 19:19:01 -0500

    updated seeds

--- backend/db/migrations/20230121172317-create-spot.js
+++ backend/db/migrations/20230121172317-create-spot.js
@@ -1,4 +1,8 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -70,9 +74,11 @@ module.exports = {
       previewImage: {
         type: Sequelize.STRING
       }
-    });
+    },
+    options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    await queryInterface.dropTable('Spots', options);
   }
 };
