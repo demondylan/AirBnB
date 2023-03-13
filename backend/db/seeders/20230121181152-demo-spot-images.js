23c961d67a267df094d4bcf72a81730faf07b60e
Dylan <gigantedylan001@yahoo.com>
2023-03-13 13:55:08 -0400

    another capitalization for id ID

--- backend/db/seeders/20230121181152-demo-spot-images.js
+++ backend/db/seeders/20230121181152-demo-spot-images.js
@@ -9,17 +9,17 @@ module.exports = {
     options.tableName = 'SpotImages';
     return queryInterface.bulkInsert(options, [
       {
-        spotid: 1,
+        spotId: 1,
         url: "https://a0.muscache.com/im/pictures/miso/Hosting-28254684/original/99bd44d1-abca-4b1c-b5da-eb05eaac9193.jpeg",
         previewImage: true
       },
       {
-        spotid: 2,
+        spotId: 2,
         url: "https://a0.muscache.com/im/pictures/miso/Hosting-21409981/original/a8fa243d-dac8-4238-93e5-f7aa33072ff8.jpeg",
         previewImage: true
       },
       {
-        spotid: 3,
+        spotId: 3,
         url: "https://a0.muscache.com/im/pictures/82c577ee-3422-4fda-ae09-6716d76e8bef.jpg",
         previewImage: true
       },
